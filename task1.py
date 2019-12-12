a8b454d6a51610882b632da8913c27a2f38acf48
psyls6 <psyls6@nottingham.ac.uk>
2019-12-12 17:15:04 +0800

    Added notebook for task1

--- task1.py
+++ task1.py
@@ -1,25 +1,61 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+from sklearn.svm import SVC
 from sklearn.decomposition import PCA
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import ShuffleSplit
+from sklearn.metrics import f1_score, accuracy_score
 from scipy import stats
 
-def myknn(sample, X_feat, Y_feat, k):
-    euclids = []
-    for i, fv in enumerate(X_feat):
-        obj = {}
-        obj["X"] = fv
-        obj["Y"] = Y_feat[i]
-        dist = [(sample[n] - fv[n]) ** 2 for n in range(len(sample))]
-        obj["dist"] = np.sqrt(sum(dist))
-        euclids.append(obj)
 
-    euclids = sorted(euclids, key = lambda i: i['dist'])
-    knn = [euclids[i]["Y"] for i in range(k)]
+class Run():
+    def __init__(self, X_train, y_train, X_test, y_test, folds=10, kernel='linear', C=1):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.folds = folds
+        self.kernel = kernel
+        self.C = C
 
-    print (knn)
+        self.main()
 
-    return stats.mode(knn).mode[0]
+
+    def main(self):
+        self.svc = SVC(kernel=self.kernel, C=self.C)
+        res = self.train()
+        f_res = self.test(self.X_test, self.y_test, "F")
+        res.append(f_res)
+        return res
+
+
+    def train(self): 
+        scores = []       
+        for i in range(self.folds):
+            X_folds = np.array_split(self.X_train, self.folds)
+            y_folds = np.array_split(self.y_train, self.folds)
+            X_val = X_folds.pop(i)
+            y_val = y_folds.pop(i)
+            X_train = np.concatenate(X_folds)
+            y_train = np.concatenate(y_folds)
+            
+            self.svc.fit(X_train, y_train)
+            y_pred = self.svc.predict(X_val)
+            scores.append(self.test(X_val, y_val, i))
+
+        return scores
+
+
+    def test(self, X, y, label):
+        y_pred = self.svc.predict(X)
+
+        res = {}
+        res['f1'] = f1_score(y, y_pred, average='micro')
+        res['accuracy'] = accuracy_score(y, y_pred)
+
+        print("{0} - f1: {1}, accuracy: {2}".format(label, res['f1'], res['accuracy']))
+        return res
 
 
 def unpickle(file):
@@ -29,48 +65,47 @@ def unpickle(file):
     return dict
 
 
-def display_img(img):
-    img2 = [[img[x], img[1024+x], img[2048+x]] for x in range(int(len(img)/3))]  
-    img3 = [img2[x*32:(x+1)*32] for x in range(32)]
-    plt.imshow(img3)
-    plt.show()
+# def display_img(img):
+#     img2 = [[img[x], img[1024+x], img[2048+x]] for x in range(int(len(img)/3))]  
+#     img3 = [img2[x*32:(x+1)*32] for x in range(32)]
+#     plt.imshow(img3)
+#     plt.show()
 
 
-def test(X_test, y_test, X_train, y_train):
-    correct = 0
-    tested = 0
-    for i in range(len(X_test)):
-        res = myknn(X_test[i], X_train, y_train, 4)
-        if res == y_test[i]:
-            res_str = "Correct  "
-            correct += 1
-        else:
-            res_str = "Incorrect"
+def load_data():
+    X_full = []
+    y_full = []
+    batch_labels = ["data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5", "test_batch"]
+    
+    for batch_label in batch_labels:
+        data = unpickle("cifar-10-batches-py/{}".format(batch_label))
+        X_full.extend(data[b"data"])
+        y_full.extend(data[b"labels"])
 
-        tested += 1
-        print ("{}/{} - {}: pred: {} actual: {}".format(correct, tested, res_str, res, y_test[i]))
+    return X_full, y_full
 
 
 def main():
-    data = unpickle("cifar-100-python/test")
-    # data[b'filenames', b'batch_label', b'fine_labels', b'coarse_labels', b'data']
+    print("Loading data")
+    X_full, y_full = load_data()
+    X_small = X_full[:1000]
+    y_small = y_full[:1000]
+    X_test = X_full[-500:]
+    y_test = y_full[-500:]
 
-    X_small = data[b"data"][:100]
-    y_small = data[b'fine_labels'][:100]
-    X_test = data[b"data"][-50:]
-    y_test = data[b'fine_labels'][-50:]
+    # X_small = [[n/255 for n in x] for x in X_small]
+    # X_test = [[n/255 for n in x] for x in X_test]
 
-    print (len(X_test))
+    print("Testing plain data..")
+    Run(X_small, y_small, X_test, y_test)
 
-    test(X_test, y_test, X_small, y_small)
+    print("Reducing dimensionality..")
+    pca = PCA(n_components=800) #New dimentionality of feature vectors
+    X_small_trans = pca.fit_transform(X_small)
+    X_test_trans = pca.transform(X_test)
 
-
-    # pca = PCA(n_components=8000)
-    # X_small_trans = pca.fit_transform(X_small)
-    # X_test_trans = pca.transform(X_test)
-
-    # print(X_small_trans[0])
-
+    print("Testing PCA data..")
+    Run(X_test_trans, y_test, X_small_trans, y_small)
 
 
 if __name__ == "__main__":
